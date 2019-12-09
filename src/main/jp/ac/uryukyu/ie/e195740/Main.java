bd22e895102a976e603ea3f7553729769ac8d8b6
SoraOoshiro <e195740@ie.u-ryukyu.ac.jp>
2019-12-09 16:56:24 +0900

    2nd commit

--- src/main/jp/ac/uryukyu/ie/e195740/Main.java
+++ src/main/jp/ac/uryukyu/ie/e195740/Main.java
@@ -3,6 +3,12 @@ package jp.ac.uryukyu.ie.e195740;
 public class Main {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        }catch(NullPointerException n){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(n.getMessage());
+        }
+
     }
 }
